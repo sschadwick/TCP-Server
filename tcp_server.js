7c45c99815bc9306fa66149ff2803979e99c94d5
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2015-09-08 20:50:52 -0700

    initial commit

---
+++ tcp_server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var net = require('net');
+var fs = require('fs');
+var rid = require('readable-id'); //creates a unique identifier
+
+exports.server = net.createServer(function(socket) {
+  var buf = '';
+  var logName = rid(); //create unique id
+
+  socket.on('data', function(data) { //fill buf with data
+    buf += data;
+  });
+
+  socket.on('end', function() {
+    fs.writeFile(__dirname + '/logs/' + logName, buf); //write buf to log
+    console.log('Request logged in: ' + logName);
+  });
+});
+
+exports.server.listen(3000);
